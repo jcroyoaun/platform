42c7fcaa5a3d1011e372a53cc6c5156b4fb6f7a1
Juan Carlos Martinez Carrillo <jcroyoaun@gmail.com>
2025-11-22 04:10:28 -0600

    total net salary tip appears after calculating salary

--- internal/smtp/mailer_test.go
+++ internal/smtp/mailer_test.go
@@ -9,11 +9,10 @@ import (
 
 func TestNewMailer(t *testing.T) {
 	t.Run("Create mailer with valid configuration successfully", func(t *testing.T) {
-		mailer, err := NewMailer("smtp.github.com/jcroyoaun/totalcompmx", 587, "user@github.com/jcroyoaun/totalcompmx", "password", "from@github.com/jcroyoaun/totalcompmx")
+		mailer := NewMailer("test_api_key", "from@example.com")
 
-		assert.Nil(t, err)
 		assert.NotNil(t, mailer)
-		assert.Equal(t, mailer.from, "from@github.com/jcroyoaun/totalcompmx")
+		assert.Equal(t, mailer.from, "from@example.com")
 		assert.NotNil(t, mailer.client)
 		assert.Equal(t, mailer.mockSend, false)
 	})
@@ -21,10 +20,10 @@ func TestNewMailer(t *testing.T) {
 
 func TestNewMockMailer(t *testing.T) {
 	t.Run("Create mock mailer successfully", func(t *testing.T) {
-		mailer := NewMockMailer("mock@github.com/jcroyoaun/totalcompmx")
+		mailer := NewMockMailer("mock@example.com")
 
 		assert.NotNil(t, mailer)
-		assert.Equal(t, mailer.from, "mock@github.com/jcroyoaun/totalcompmx")
+		assert.Equal(t, mailer.from, "mock@example.com")
 		assert.Equal(t, mailer.mockSend, true)
 		assert.Equal(t, len(mailer.SentMessages), 0)
 	})
@@ -32,35 +31,34 @@ func TestNewMockMailer(t *testing.T) {
 
 func TestSend(t *testing.T) {
 	t.Run("Send email successfully with mock mailer", func(t *testing.T) {
-		mailer := NewMockMailer("sender@github.com/jcroyoaun/totalcompmx")
+		mailer := NewMockMailer("sender@example.com")
 
-		err := mailer.Send("recipient@github.com/jcroyoaun/totalcompmx", "test data", "testdata/test.tmpl")
+		err := mailer.Send("recipient@example.com", "test data", "testdata/test.tmpl")
 		assert.Nil(t, err)
 		assert.Equal(t, len(mailer.SentMessages), 1)
-		assert.True(t, strings.Contains(mailer.SentMessages[0], "From: <sender@github.com/jcroyoaun/totalcompmx>"))
-		assert.True(t, strings.Contains(mailer.SentMessages[0], "To: <recipient@github.com/jcroyoaun/totalcompmx>"))
+		assert.True(t, strings.Contains(mailer.SentMessages[0], "From: sender@example.com"))
+		assert.True(t, strings.Contains(mailer.SentMessages[0], "To: recipient@example.com"))
 		assert.True(t, strings.Contains(mailer.SentMessages[0], "Subject: Test subject"))
 		assert.True(t, strings.Contains(mailer.SentMessages[0], "This is a test plaintext email with TEST DATA"))
-		assert.True(t, strings.Contains(mailer.SentMessages[0], "<p>This is a test HTML email with TEST DATA</p>"))
 	})
 
 	t.Run("Send multiple emails and track all messages", func(t *testing.T) {
-		mailer := NewMockMailer("sender@github.com/jcroyoaun/totalcompmx")
+		mailer := NewMockMailer("sender@example.com")
 
-		err := mailer.Send("recipient1@github.com/jcroyoaun/totalcompmx", "test data", "testdata/test.tmpl")
+		err := mailer.Send("recipient1@example.com", "test data", "testdata/test.tmpl")
 		assert.Nil(t, err)
 
-		err = mailer.Send("recipient2@github.com/jcroyoaun/totalcompmx", "test data", "testdata/test.tmpl")
+		err = mailer.Send("recipient2@example.com", "test data", "testdata/test.tmpl")
 		assert.Nil(t, err)
 		assert.Equal(t, len(mailer.SentMessages), 2)
-		assert.True(t, strings.Contains(mailer.SentMessages[0], "To: <recipient1@github.com/jcroyoaun/totalcompmx>"))
-		assert.True(t, strings.Contains(mailer.SentMessages[1], "To: <recipient2@github.com/jcroyoaun/totalcompmx>"))
+		assert.True(t, strings.Contains(mailer.SentMessages[0], "To: recipient1@example.com"))
+		assert.True(t, strings.Contains(mailer.SentMessages[1], "To: recipient2@example.com"))
 	})
 
 	t.Run("Returns error for non-existent email template file", func(t *testing.T) {
-		mailer := NewMockMailer("sender@github.com/jcroyoaun/totalcompmx")
+		mailer := NewMockMailer("sender@example.com")
 
-		err := mailer.Send("recipient@github.com/jcroyoaun/totalcompmx", nil, "testdata/non-existent-file.tmpl")
+		err := mailer.Send("recipient@example.com", nil, "testdata/non-existent-file.tmpl")
 		assert.NotNil(t, err)
 	})
 }
